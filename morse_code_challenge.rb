7e58b9361181c2bb39f4100243e9c7e9f9ed1034
Ruthmy <ruthmy17@gmail.com>
2023-08-24 12:54:16 -0600

    Modified 'morse_code_challenge' - Added the functions 'decode_word' and 'decode_word' - Created some examples to test functionality

--- morse_code_challenge.rb
+++ morse_code_challenge.rb
@@ -1,3 +1,4 @@
+# Decode a character
 def decode_char(character)
   morse_alphabet = {
     '.-' => 'A', '-...' => 'B', '-.-.' => 'C', '-..' => 'D', '.' => 'E', '..-.' => 'F', '--.' => 'G', '....' => 'H',
@@ -8,6 +9,24 @@ def decode_char(character)
   morse_alphabet[character]
 end
 
-# Decode a character
+# Decode the character A
 character = decode_char('.-')
 puts character
+
+# Decode a word
+def decode_word(word)
+  word.split.map { |character| decode_char(character) }.join
+end
+
+# Argument sent HELLO
+word = decode_word('.... . .-.. .-.. ---')
+puts word
+
+# Decode a whole sentence
+def decode_sentence(sentence)
+  sentence.split('   ').map { |word| decode_word(word) }.join(' ')
+end
+
+# Argument sent HELLO WORLD
+sentence = decode_sentence('.... . .-.. .-.. ---   .-- --- .-. .-.. -..')
+puts sentence
