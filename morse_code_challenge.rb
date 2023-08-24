51bb2d42289df3b06afeb43b8f889de9d98c4dbc
Carmen Bravo <nbravoalcala@gmail.com>
2023-08-24 12:55:45 -0400

    Create the decode_char function to convert a character from morse to letter

---
+++ morse_code_challenge.rb
@@ -0,0 +1,13 @@
+def decode_char(character)
+  morse_alphabet = {
+    '.-' => 'A', '-...' => 'B', '-.-.' => 'C', '-..' => 'D', '.' => 'E', '..-.' => 'F', '--.' => 'G', '....' => 'H',
+    '..' => 'I', '.---' => 'J', '-.-' => 'K', '.-..' => 'L', '--' => 'M', '-.' => 'N', '---' => 'O', '.--.' => 'P',
+    '--.-' => 'Q', '.-.' => 'R', '...' => 'S', '-' => 'T', '..-' => 'U', '...-' => 'V', '.--' => 'W', '-..-' => 'X',
+    '-.--' => 'Y', '--..' => 'Z'
+  }
+  morse_alphabet[character]
+end
+
+# Decode a character
+character = decode_char('.-')
+puts character
